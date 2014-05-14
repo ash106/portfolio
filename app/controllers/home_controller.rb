e6027c32e3762f3914843ddafa4666622760cd29
Alex Howington <alex.howington@gmail.com>
2014-05-13 18:20:10 -0600

    Add basic static page templates

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def portfolio
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'projects#index'
+  root 'home#portfolio'
+  get 'portfolio', to: 'home#portfolio', as: :portfolio
+  get 'about', to: 'home#about', as: :about
+  get 'contact', to: 'home#contact', as: :contact
   devise_for :users
   resources :users
   resources :projects
