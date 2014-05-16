26fdf53480e4de45a13e7fd3a959c13f2f6f65ff
Alex Howington <alex.howington@gmail.com>
2014-05-15 19:36:41 -0600

    Add size as parameter to get_main_image Project helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,8 +1,8 @@
 module ProjectsHelper
 
-  def get_main_image(project)
+  def get_main_image(project, size)
     if picture = project.pictures.find_by(title: 'Main')
-      picture.image.url(:medium)
+      picture.image.url(size)
     else
       '/images/missing_medium.png'
     end
